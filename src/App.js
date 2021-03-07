d49d48222c94f36cf00de15755fa0d6f9890af7b
Jakob Parakenings <jakobparakenings@Jakobs-MacBook-Pro.local>
2021-03-07 21:17:33 +0100

    Verlinkungen im HTML Code hinzugefügt. Überrauschung bei 'Hier klicken.' eingebettet.

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
       <main class="Site-content">
         <body class="Site">
           <div class="head_container">
-          <div class="banner">Anwalt oder Anwältin? Hier&nbsp <a class="klicken_btn" href="anwalt_anmeldung.html">klicken.</a></div>
+          <div class="banner">Anwalt oder Anwältin? Hier&nbsp <a class="klicken_btn" href="https://bit.ly/3eijYny">klicken.</a></div>
           <div class="header">
             <div class="logo"><a href="index.html"><img src="./Logo_AdvoNow_zugeschnitten.png" alt="Hier müsste das Logo sein." height="60px" width="300px"></img></a></div>
             <div class="header_btn">
-              <div class="about"><a href="about.html"></a>ABOUT</div>
-              <div class="services"><a href="services">SERVICES</a></div>
-              <div class="contact"><a href="contact"></a>KONTAKT</div>
+              <div class="about"><a href="about.html">ABOUT</a></div>
+              <div class="services"><a href="services.html">SERVICES</a></div>
+              <div class="contact"><a href="contact.html">KONTAKT</a></div>
             </div>
           </div>
           </div>
@@ -25,9 +25,9 @@ function App() {
       <footer>
         <div class="footer_container">
           <div class="footer_links">
-            <div class="AGB">AGB</div>
-            <div class="Impressum">Impressum</div>
-            <div class="Datenschutz">Datenschutz</div>
+            <div class="AGB"><a href="agb.html">AGB</a></div>
+            <div class="Impressum"><a href="impressum.html">Impressum</a></div>
+            <div class="Datenschutz"><a href="datenschutz.html">Datenschutz</a></div>
           </div>
         </div>
       </footer>
